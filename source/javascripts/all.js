622878a67e60df7722eebb2e0e2918accb2eeb9c
Dennis Schipper <dennis@designfordetails.com>
2015-11-05 13:40:01 +0000

    add jquery to the page, basic code reveal functionality and some fancy link underlines with animation and :after

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,12 @@
 //= require_tree .
+
+
+$(document).ready(function() {
+
+  $(".reveal-code").click(function() {
+    var code = $(this).parent().find(".code")
+
+    code.toggle();
+    return false;
+  })
+})
