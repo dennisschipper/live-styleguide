5a46d7d7df2856dc86a291ef415a4d080313e9a2
Dennis Schipper <dennis@designfordetails.com>
2015-11-07 14:31:48 +0000

    remove jquery, add resize js for the sidebar

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,12 +1,24 @@
 //= require_tree .
 
+// Wait until the content is loaded
+document.addEventListener("DOMContentLoaded", function(event) {
 
-$(document).ready(function() {
+  // Set some generic variables for resizing later
+  var window_height = window.innerHeight;
+  var sidebar = document.getElementsByTagName("aside")[0];
+  sidebar.style.height = window_height + "px";
 
-  $(".reveal-code").click(function() {
-    var code = $(this).parent().find(".code")
 
-    code.toggle();
-    return false;
+  // Lets set the sidebar at the same height as the window
+  function resize_sidebar() {
+    var window_height = window.innerHeight;
+    sidebar.style.height = window_height + "px";
+    return "done!"
+  }
+  resize_sidebar();
+
+  // On resize, do things
+  window.addEventListener('resize', function(event){
+    resize_sidebar();
   })
 })
